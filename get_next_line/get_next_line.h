a6f767bb684ab9eefea9ad2b089fb2d8240a365c
Jung kyu Lee <48317269+ljk9410@users.noreply.github.com>
2020-11-23 15:59:42 +0900

    fix

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -12,9 +12,10 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-// # include <unistd.h>
+# include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
+int     get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
