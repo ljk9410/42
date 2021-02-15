bfb14e3cdf57cd5cb4d5a26d966969cc19d49712
Jung kyu Lee <jung-lee@c2r8s1.42seoul.kr>
2021-02-15 20:32:29 +0900

    add 's'

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,17 +6,20 @@
 /*   By: jung-lee <jung-lee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 21:51:27 by jung-lee          #+#    #+#             */
-/*   Updated: 2021/02/15 15:42:03 by jung-lee         ###   ########.fr       */
+/*   Updated: 2021/02/15 20:32:00 by jung-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <string.h> // 지워야 되는 헤더
 
 int     ft_printf(const char *format, ...)
 {
     va_list     ap;
     int         i;
+    int         len;
     char        var;
+    char        *str;
 
     va_start(ap, format);
     i = 0;
@@ -32,8 +35,17 @@ int     ft_printf(const char *format, ...)
                 write(1, &var, 1);
                 i++;
             }
+            if (format[i + 1] == 's')
+            {
+                str = va_arg(ap, char *);
+                len = strlen(str);
+                for (int j = 0; j < len; j++) // 수정해야 할 부분
+                    write(1, &str[j], 1);
+                i++;
+            }
         }
         i++;
     }
+    va_end(ap);
     return (0);
 }
