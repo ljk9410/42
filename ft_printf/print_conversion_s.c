6e27f853ec24b2ffb7ccb57615b41bda96ecdd63
hyeyeon2964 <hyeyeon2964@hanmail.net>
2021-03-05 16:28:09 +0900

    modify s

--- ft_printf/print_conversion_s.c
+++ ft_printf/print_conversion_s.c
@@ -6,7 +6,7 @@
 /*   By: jung-lee <jung-lee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 15:06:51 by jung-lee          #+#    #+#             */
-/*   Updated: 2021/03/02 15:43:10 by hmyoung          ###   ########.fr       */
+/*   Updated: 2021/03/05 16:26:44 by hmyoung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,8 @@ static int	check_width(int len)
 {
 	int	check_w;
 
+	if (g_opts.dot == 1 && g_opts.precision <= len)
+		len = g_opts.precision;
 	if (g_opts.width <= len)
 		check_w = 0;
 	else
@@ -72,6 +74,8 @@ void		print_conversion_s(va_list ap)
 	check_w = 0;
 	handle_wp_star(ap);
 	str = va_arg(ap, char *);
+	if (str == NULL)
+		str = "(null)";
 	len = ft_strlen(str);
 	check_p = check_precision(len);
 	check_w = check_width(len);
