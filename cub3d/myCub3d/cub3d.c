767a63b46a271dd672ed916af48f9f4f2b164f96
Jung kyu Lee <jung-lee@c5r8s7.42seoul.kr>
2021-04-29 15:26:28 +0900

    add resolution func

--- cub3d/myCub3d/cub3d.c
+++ cub3d/myCub3d/cub3d.c
@@ -6,27 +6,53 @@
 /*   By: jung-lee <jung-lee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 15:51:41 by jung-lee          #+#    #+#             */
-/*   Updated: 2021/04/27 17:11:20 by jung-lee         ###   ########.fr       */
+/*   Updated: 2021/04/29 15:13:02 by jung-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./cub3d.h"
 
-int		main()
+void	save_resolution(t_info *info, char *str)
+{
+	char	**res;
+
+	res = ft_split(str, ' ');
+	info->win_w = ft_atoi(res[1]);
+	info->win_h = ft_atoi(res[2]);
+}
+
+void	save_texture(t_info *info, char *str)
+{
+	char 	**res;
+
+	res = ft_split(str, ' ');
+	printf("%s\n", res[1]);
+}
+
+int		parsing_map(t_info *info, char *argv)
 {
 	int		fd;
 	int		res;
 	char	*str;
-	
-	fd = open("./maps/map.cub", O_RDONLY);
-	
-	
+
+	fd = open(argv, O_RDONLY);
 	while ((res = get_next_line(fd, &str)) != -1)
 	{
-		printf("%s\n", str);
+		if (str[0] == 'R')
+			save_resolution(info, str);
+		if (str[0] == 'N' || str[0] == 'S' || str[0] == 'W' || str[0] == 'E')
+			save_texture(info, str);
 		if (res == 0)
 			break ;
 	}
-	
+	return (0);
+}
+
+int		main(int argc, char **argv)
+{
+	t_info	info;
+	int		temp;
+
+	temp = parsing_map(&info, argv[1]);
 	return (0);
 }
--- cub3d/myCub3d/cub3d.h
+++ cub3d/myCub3d/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: jung-lee <jung-lee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/27 15:52:04 by jung-lee          #+#    #+#             */
-/*   Updated: 2021/04/27 16:37:32 by jung-lee         ###   ########.fr       */
+/*   Updated: 2021/04/29 15:17:28 by jung-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,31 @@
 # include <fcntl.h>
 # include <stdio.h>
 
+typedef	struct	s_info
+{
+	int			win_w;
+	int			win_h;
+}				t_info;
+
+typedef struct	s_img
+{
+	void		*img;
+	int			*data;
+	int			bpp;
+	int			size_l;
+	int			endian;
+	int			img_w;
+	int			img_h;
+}				t_img;
+
 int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *str);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *str, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-
+char	**ft_split(char const *str, char c);
+int		ft_atoi(const char *str);
 #endif
 #ifndef BUFFER_SIZE
 # define BUFFER_SIZE 10
