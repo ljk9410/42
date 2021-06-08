0e628eec3e2d7e7c3fadd47764a086fdc1aa9e39
Jung kyu Lee <jung-lee@c5r6s5.42seoul.kr>
2021-06-08 20:18:26 +0900

    lst init fin

--- push_swap/srcs/lst_function.c
+++ push_swap/srcs/lst_function.c
@@ -6,10 +6,32 @@
 /*   By: jung-lee <jung-lee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 15:43:39 by jung-lee          #+#    #+#             */
-/*   Updated: 2021/06/01 15:44:23 by jung-lee         ###   ########.fr       */
+/*   Updated: 2021/06/08 20:13:20 by jung-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/push_swqp.h"
+#include "../includes/push_swap.h"
 
+t_struct	*find_lst_last(t_struct *node)
+{
+	t_struct	*temp;
 
+	temp = node;
+	if (!temp)
+		return (NULL);
+	while (temp->next != NULL)
+		temp = temp->next;
+	return (temp);
+}
+
+void		add_lst_back(t_struct *node, int n)
+{
+	t_struct	*new;
+	t_struct	*temp;
+
+	new = (t_struct *)malloc(sizeof(t_struct));
+	temp = find_lst_last(node);
+	temp->next = new;
+	new->value = n;
+	new->next = NULL;
+}
--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -6,42 +6,18 @@
 /*   By: jung-lee <jung-lee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 13:28:44 by jung-lee          #+#    #+#             */
-/*   Updated: 2021/06/08 20:04:11 by jung-lee         ###   ########.fr       */
+/*   Updated: 2021/06/08 20:17:50 by jung-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
-#include <stdio.h>
 
-/*
-int		error_check(int argc, char **argv)
-*/
-
-t_struct	*find_lst_last(t_struct *node)
+int				error_check(int argc, char **argv)
 {
-	t_struct	*temp;
-
-	temp = node;
-	if (!temp)
-		return (NULL);
-	while (temp->next != NULL)
-		temp = temp->next;
-	return (temp);
+	return (0);
 }
 
-void		add_lst_back(t_struct *node, int n)
-{
-	t_struct	*new;
-	t_struct	*temp;
-
-	new = (t_struct *)malloc(sizeof(t_struct));
-	temp = find_lst_last(node);
-	temp->next = new;
-	new->value = n;
-	new->next = NULL;
-}
-
-void		init_stack(t_struct *head_A, int argc, char **argv)
+void			init_stack(t_struct *head_a, int argc, char **argv)
 {
 	int			i;
 	int			n;
@@ -51,20 +27,19 @@ void		init_stack(t_struct *head_A, int argc, char **argv)
 	while (i < argc)
 	{
 		n = ft_atoi(argv[i]);
-		add_lst_back(head_A, n);
+		add_lst_back(head_a, n);
 		i++;
 	}
-
 }
 
-int			main(int argc, char **argv)
-{	
-	t_struct 	*head_A;
-	t_struct 	*head_B;
+int				main(int argc, char **argv)
+{
+	t_struct	*head_a;
+	t_struct	*head_b;
 
-	//error_check(argc, argv);
-	head_A = (t_struct *)malloc(sizeof(t_struct));
-	head_B = (t_struct *)malloc(sizeof(t_struct));
-	init_stack(head_A, argc, argv);
+	error_check(argc, argv);
+	head_a = (t_struct *)malloc(sizeof(t_struct));
+	head_a = (t_struct *)malloc(sizeof(t_struct));
+	init_stack(head_a, argc, argv);
 	return (0);
 }
