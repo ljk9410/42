18f029aa69e622d3d19c13fd35090411e71b5af7
Jung kyu Lee <jung-lee@c5r3s5.42seoul.kr>
2021-07-02 16:08:14 +0900

    norm check headerfile

--- push_swap/srcs/handle_operation_list.c
+++ push_swap/srcs/handle_operation_list.c
@@ -6,28 +6,14 @@
 /*   By: jung-lee <jung-lee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/01 12:33:24 by jung-lee          #+#    #+#             */
-/*   Updated: 2021/07/02 12:24:54 by jung-lee         ###   ########.fr       */
+/*   Updated: 2021/07/02 16:04:31 by jung-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void			handle_operation_list(t_oper *op)
+void            handle_operation_list(t_oper *op)
 {
-	t_oper		*temp;
-	size_t		len;
-
-	temp = op->next;
-	// operation list 최적화 함수
-	// 최적화된 리스트 출력
-	while (temp != NULL)
-	{
-		len = ft_strlen(temp->operation);
-		if (len == 2)
-			write(1, temp->operation, 2);
-		else
-			write(1, temp->operation, 3);
-		write(1, "\n", 1);
-		temp = temp->next;
-	}
+//    optimize_operation(op);
+//    print_op_list(op);
 }
