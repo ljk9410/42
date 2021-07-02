673927b972c02248244cfa331b1f13ed4f2259e8
Jung kyu Lee <jung-lee@c5r3s5.42seoul.kr>
2021-07-02 14:51:39 +0900

    trim code in sort_function

--- push_swap/srcs/sort_function.c
+++ push_swap/srcs/sort_function.c
@@ -1,36 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_function.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jung-lee <jung-lee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/02 14:46:00 by jung-lee          #+#    #+#             */
+/*   Updated: 2021/07/02 14:49:48 by jung-lee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/push_swap.h"
-
-int	find_max(t_struct *head)
-{
-	int			max;
-	t_struct	*temp;
-
-	temp = head->next;
-	max = temp->value;
-	while (temp != NULL)
-	{
-		if (max < temp->value)
-			max = temp->value;
-		temp = temp->next;
-	}
-	return (max);
-}
-
-int	find_min(t_struct *head)
-{
-	int			min;
-	t_struct	*temp;
-
-	temp = head->next;
-	min = temp->value;
-	while (temp != NULL)
-	{
-		if (min > temp->value)
-			min = temp->value;
-		temp = temp->next;
-	}
-	return (min);
-}
 
 void	sort_two_a(t_struct *head_a, t_oper *op)
 {
@@ -63,16 +43,13 @@ void	sort_three_a(t_struct *head_a, t_oper *op)
 	max = find_max(head_a);
 	while (temp->value != max)
 		temp = temp->next;
-	//최대값이 마지막에 있을 때
 	if (temp->next == NULL)
 		sort_two_a(head_a, op);
-	//최대값이 두 번째에 있을 때
 	else if (temp->next->next == NULL)
 	{
 		rra(head_a, op);
 		sort_two_a(head_a, op);
 	}
-	//최대값이 제일 위에 있을 때
 	else
 	{
 		ra(head_a, op);
@@ -103,24 +80,11 @@ void	sort_three_b(t_struct *head_b, t_oper *op)
 	}
 }
 
-void	check_list(t_struct *node)
-{
-	t_struct *temp;
-
-	temp = node->next;
-	printf("list: ");
-	while (temp != NULL)
-	{
-		printf("%d ", temp->value);
-		temp = temp->next;
-	}
-}
-
 void	sort_five(t_struct *head_a, t_struct *head_b, int len, t_oper *op)
 {
 	int			middle;
 	int			i;
-	
+
 	i = len;
 	middle = check_pivot(head_a, len);
 	while (i--)
